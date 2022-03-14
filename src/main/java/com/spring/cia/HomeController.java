2ad1566bd7d996c6c2bf2961a1f073bd8fbda212
hyunfromseoul <hongjae30@gmail.com>
2022-03-14 15:20:30 +0900

    #3 end

--- src/main/java/com/spring/cia/HomeController.java
+++ src/main/java/com/spring/cia/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		String a = "3#asddd#";
+		String a = "3#asdddasdasd#";
 		return "home";
 	}
 	
