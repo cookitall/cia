762d79347cf033fc8bb127d696b6bf36c9abc6ae
hs923 <hs923@PHS-DESKTOP>
2022-03-25 16:23:22 +0900

    # phs 상점 화면 리뷰 목록 출력

--- src/main/java/com/spring/cia/shop/service/ShopService.java
+++ src/main/java/com/spring/cia/shop/service/ShopService.java
@@ -1,10 +1,12 @@
 package com.spring.cia.shop.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.cia.command.ReviewVO;
 import com.spring.cia.command.ShopVO;
 import com.spring.cia.shop.mapper.IShopMapper;
 import com.spring.cia.util.PageVO;
@@ -28,6 +30,11 @@ public class ShopService implements IShopService {
 	@Override
 	public ShopVO getShopInfo(String shopName) {
 		return mapper.getShopInfo(shopName);
+	}
+	
+	@Override
+	public List<ReviewVO> getReviewReplyList(String shopName) {
+		return mapper.getReviewReplyList(shopName);
 	}
 
 
